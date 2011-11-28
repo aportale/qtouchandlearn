5b75c7d9ff5a72a4eb6e73b34d2c2a58ef8b6b21
Alessandro Portale <alessandro@casaportale.de>
2011-11-29 00:58:47 +0100

    Added a "exerciseImages" benchmark.

--- test/renderspeed/tst_renderspeedtest.cpp
+++ test/renderspeed/tst_renderspeedtest.cpp
@@ -13,6 +13,8 @@ public:
 
 private Q_SLOTS:
     void vignetteEffect();
+    void exerciseImages();
+    void exerciseImages_data();
 
 private:
     ImageProvider m_imageProvider;
@@ -35,6 +37,27 @@ void RenderspeedTest::vignetteEffect()
         m_imageProvider.requestPixmap(QLatin1String("button/1"), &size, requestedButtonSize);
         m_imageProvider.requestPixmap(QLatin1String("button/2"), &size, requestedButtonSize);
     }
+}
+
+void RenderspeedTest::exerciseImages()
+{
+    QFETCH(QString, id);
+    // Simulating a 360 x 640 pixels screen size
+    const QSize requestedFrameSize(360, 322);
+    QSize size;
+    QBENCHMARK {
+        m_imageProvider.requestPixmap(id, &size, requestedFrameSize);
+    }
+}
+
+void RenderspeedTest::exerciseImages_data()
+{
+    QTest::addColumn<QString>("id");
+    QTest::newRow("Read (Robot)") << QString::fromLatin1("object/robot");
+    QTest::newRow("Count (20 Fishes)") << QString::fromLatin1("quantity/20/fish");
+    QTest::newRow("Clock") << QString::fromLatin1("clock/9/45/0");
+    QTest::newRow("Music (a sharp)") << QString::fromLatin1("notes/a sharp");
+    QTest::newRow("Color (Yellow)") << QString::fromLatin1("color/#FF0/0");
 }
 
 QTEST_MAIN(RenderspeedTest)
