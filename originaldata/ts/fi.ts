27a8dea45f8f012ddad2a7fc1adb931544ad0136
Alessandro Portale <alessandro@casaportale.de>
2014-07-08 17:07:36 +0200

    Finnish translation by Tommi Kanerva
    Thank you, Tommi! :)

---
+++ originaldata/ts/fi.ts
@@ -0,0 +1,391 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="fi" sourcelanguage="en_US">
+<context>
+    <name>Colors</name>
+    <message>
+        <source>red</source>
+        <translation>punainen</translation>
+    </message>
+    <message>
+        <source>green</source>
+        <translation>vihreä</translation>
+    </message>
+    <message>
+        <source>blue</source>
+        <translation>sininen</translation>
+    </message>
+    <message>
+        <source>white</source>
+        <translation>valkoinen</translation>
+    </message>
+    <message>
+        <source>gray</source>
+        <translation>harmaa</translation>
+    </message>
+    <message>
+        <source>black</source>
+        <translation>musta</translation>
+    </message>
+    <message>
+        <source>yellow</source>
+        <translation>keltainen</translation>
+    </message>
+    <message>
+        <source>orange</source>
+        <translation>oranssi</translation>
+    </message>
+    <message>
+        <source>brown</source>
+        <translation>ruskea</translation>
+    </message>
+    <message>
+        <source>pink</source>
+        <translation>vaaleanpunainen</translation>
+    </message>
+    <message>
+        <source>violet</source>
+        <translation>violetti</translation>
+    </message>
+</context>
+<context>
+    <name>LessonMenu</name>
+    <message>
+        <source>Read the first letter</source>
+        <translation>Ensimmäinen kirjain</translation>
+    </message>
+    <message>
+        <source>Read words</source>
+        <translation>Sanojen lukeminen</translation>
+    </message>
+    <message>
+        <source>Count to 5</source>
+        <translation>Laske viiteen</translation>
+    </message>
+    <message>
+        <source>Count and read to 5</source>
+        <translation>Laske ja lue viiteen</translation>
+    </message>
+    <message>
+        <source>Count to 20</source>
+        <translation>Laske kahteenkymmeneen</translation>
+    </message>
+    <message>
+        <source>Count and read to 20</source>
+        <translation>Laske ja lue kahteenkymmeneen</translation>
+    </message>
+    <message>
+        <source>Read the clock, full hours</source>
+        <translation>Kellon lukeminen, täydet tunnit</translation>
+    </message>
+    <message>
+        <source>Read the clock, half hours</source>
+        <translation>Kellon lukeminen, puolet-tunnit</translation>
+    </message>
+    <message>
+        <source>Read the clock</source>
+        <translation>Kellon lukeminen</translation>
+    </message>
+    <message>
+        <source>Read</source>
+        <translation>Lukeminen</translation>
+    </message>
+    <message>
+        <source>Count</source>
+        <translation>Laskeminen</translation>
+    </message>
+    <message>
+        <source>Clock</source>
+        <translation>Kello</translation>
+    </message>
+    <message>
+        <source>Music</source>
+        <translation>Musiikki</translation>
+    </message>
+    <message>
+        <source>Read notes, whole step</source>
+        <translation>Lue nuotteja, kokosävelaskel</translation>
+    </message>
+    <message>
+        <source>Read notes, half-step</source>
+        <translation>Lue nuotteja, puolisävelaskel</translation>
+    </message>
+    <message>
+        <source>Mixed</source>
+        <translation>Sekalainen</translation>
+    </message>
+    <message>
+        <source>Mixed lessons, easy</source>
+        <translation>Sekalainen harjoitus, helppo</translation>
+    </message>
+    <message>
+        <source>Mixed lessons, medium</source>
+        <translation>Sekalainen harjoitus, keskitaso</translation>
+    </message>
+    <message>
+        <source>Mixed lessons, hard</source>
+        <translation>Sekalainen harjoitus, vaikea</translation>
+    </message>
+    <message>
+        <source>Color</source>
+        <translation>Väri</translation>
+    </message>
+    <message>
+        <source>Recognize the color</source>
+        <translation>Tunnista väri</translation>
+    </message>
+</context>
+<context>
+    <name>Notes</name>
+    <message>
+        <source>C</source>
+        <translation>c</translation>
+    </message>
+    <message>
+        <source>C sharp</source>
+        <translation>cis</translation>
+    </message>
+    <message>
+        <source>D flat</source>
+        <translation>des</translation>
+    </message>
+    <message>
+        <source>D</source>
+        <translation>d</translation>
+    </message>
+    <message>
+        <source>D sharp</source>
+        <translation>dis</translation>
+    </message>
+    <message>
+        <source>E flat</source>
+        <translation>es</translation>
+    </message>
+    <message>
+        <source>E</source>
+        <translation>e</translation>
+    </message>
+    <message>
+        <source>F flat</source>
+        <translation>fes</translation>
+    </message>
+    <message>
+        <source>E sharp</source>
+        <translation>eis</translation>
+    </message>
+    <message>
+        <source>F</source>
+        <translation>f</translation>
+    </message>
+    <message>
+        <source>F sharp</source>
+        <translation>fis</translation>
+    </message>
+    <message>
+        <source>G flat</source>
+        <translation>ges</translation>
+    </message>
+    <message>
+        <source>G</source>
+        <translation>g</translation>
+    </message>
+    <message>
+        <source>G sharp</source>
+        <translation>gis</translation>
+    </message>
+    <message>
+        <source>A flat</source>
+        <translation>as</translation>
+    </message>
+    <message>
+        <source>A</source>
+        <translation>a</translation>
+    </message>
+    <message>
+        <source>A sharp</source>
+        <translation>ais</translation>
+    </message>
+    <message>
+        <source>B flat</source>
+        <translation>bb</translation>
+    </message>
+    <message>
+        <source>B</source>
+        <translation>h</translation>
+    </message>
+    <message>
+        <source>C flat</source>
+        <translation>ces</translation>
+    </message>
+</context>
+<context>
+    <name>Numbers</name>
+    <message>
+        <source>zero</source>
+        <translation>nolla</translation>
+    </message>
+    <message>
+        <source>one</source>
+        <translation>yksi</translation>
+    </message>
+    <message>
+        <source>two</source>
+        <translation>kaksi</translation>
+    </message>
+    <message>
+        <source>three</source>
+        <translation>kolme</translation>
+    </message>
+    <message>
+        <source>four</source>
+        <translation>neljä</translation>
+    </message>
+    <message>
+        <source>five</source>
+        <translation>viisi</translation>
+    </message>
+    <message>
+        <source>six</source>
+        <translation>kuusi</translation>
+    </message>
+    <message>
+        <source>seven</source>
+        <translation>seitsemän</translation>
+    </message>
+    <message>
+        <source>eight</source>
+        <translation>kahdeksan</translation>
+    </message>
+    <message>
+        <source>nine</source>
+        <translation>yhdeksän</translation>
+    </message>
+    <message>
+        <source>ten</source>
+        <translation>kymmenen</translation>
+    </message>
+    <message>
+        <source>eleven</source>
+        <translation>yksitoista</translation>
+    </message>
+    <message>
+        <source>twelve</source>
+        <translation>kaksitoista</translation>
+    </message>
+    <message>
+        <source>thirteen</source>
+        <translation>kolmetoista</translation>
+    </message>
+    <message>
+        <source>fourteen</source>
+        <translation>neljätoista</translation>
+    </message>
+    <message>
+        <source>fifteen</source>
+        <translation>viisitoista</translation>
+    </message>
+    <message>
+        <source>sixteen</source>
+        <translation>kuusitoista</translation>
+    </message>
+    <message>
+        <source>seventeen</source>
+        <translation>seitsemäntoista</translation>
+    </message>
+    <message>
+        <source>eighteen</source>
+        <translation>kahdeksantoista</translation>
+    </message>
+    <message>
+        <source>nineteen</source>
+        <translation>yhdeksäntoista</translation>
+    </message>
+    <message>
+        <source>twenty</source>
+        <translation>kaksikymmentä</translation>
+    </message>
+</context>
+<context>
+    <name>Objects</name>
+    <message>
+        <source>banana</source>
+        <translation>banaani</translation>
+    </message>
+    <message>
+        <source>elephant</source>
+        <translation>norsu</translation>
+    </message>
+    <message>
+        <source>robot</source>
+        <translation>robotti</translation>
+    </message>
+    <message>
+        <source>flower</source>
+        <translation>kukka</translation>
+    </message>
+    <message>
+        <source>fish</source>
+        <translation>kala</translation>
+    </message>
+    <message>
+        <source>rooster</source>
+        <translation>kukko</translation>
+    </message>
+    <message>
+        <source>airplane</source>
+        <translation>lentokone</translation>
+    </message>
+    <message>
+        <source>candle</source>
+        <translation>kynttilä</translation>
+    </message>
+    <message>
+        <source>scissors</source>
+        <translation>sakset</translation>
+    </message>
+    <message>
+        <source>key</source>
+        <translation>avain</translation>
+    </message>
+    <message>
+        <source>horse</source>
+        <translation>hevonen</translation>
+    </message>
+    <message>
+        <source>dog</source>
+        <translation>koira</translation>
+    </message>
+    <message>
+        <source>cat</source>
+        <translation>kissa</translation>
+    </message>
+    <message>
+        <source>camel</source>
+        <translation>kameli</translation>
+    </message>
+    <message>
+        <source>crocodile</source>
+        <translation>krokotiili</translation>
+    </message>
+    <message>
+        <source>pig</source>
+        <translation>sika</translation>
+    </message>
+    <message>
+        <source>snake</source>
+        <translation>käärme</translation>
+    </message>
+    <message>
+        <source>giraffe</source>
+        <translation>kirahvi</translation>
+    </message>
+    <message>
+        <source>snail</source>
+        <translation>etana</translation>
+    </message>
+    <message>
+        <source>hedgehog</source>
+        <translation>siili</translation>
+    </message>
+</context>
+</TS>
