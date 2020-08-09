9fbf08380d9f7f5193f3fa5b711a0f6a33f82935
luxferre <abimakov@gmail.com>
2020-08-10 00:00:18 +0400

    add russian translation

---
+++ originaldata/ts/ru.ts
@@ -0,0 +1,391 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ru" sourcelanguage="en_US">
+<context>
+    <name>Colors</name>
+    <message>
+        <source>red</source>
+        <translation>красный</translation>
+    </message>
+    <message>
+        <source>green</source>
+        <translation>зелёный</translation>
+    </message>
+    <message>
+        <source>blue</source>
+        <translation>синий</translation>
+    </message>
+    <message>
+        <source>white</source>
+        <translation>белый</translation>
+    </message>
+    <message>
+        <source>gray</source>
+        <translation>серый</translation>
+    </message>
+    <message>
+        <source>black</source>
+        <translation>чёрный</translation>
+    </message>
+    <message>
+        <source>yellow</source>
+        <translation>жёлтый</translation>
+    </message>
+    <message>
+        <source>orange</source>
+        <translation>оранжевый</translation>
+    </message>
+    <message>
+        <source>brown</source>
+        <translation>коричневый</translation>
+    </message>
+    <message>
+        <source>pink</source>
+        <translation>розовый</translation>
+    </message>
+    <message>
+        <source>violet</source>
+        <translation>фиолетовый</translation>
+    </message>
+</context>
+<context>
+    <name>LessonMenu</name>
+    <message>
+        <source>Read the first letter</source>
+        <translation>Прочитай первую букву</translation>
+    </message>
+    <message>
+        <source>Read words</source>
+        <translation>Прочитай слова</translation>
+    </message>
+    <message>
+        <source>Count to 5</source>
+        <translation>Сосчитай до 5</translation>
+    </message>
+    <message>
+        <source>Count and read to 5</source>
+        <translation>Сосчитай до 5 и прочитай</translation>
+    </message>
+    <message>
+        <source>Count to 20</source>
+        <translation>Сосчитай до 20</translation>
+    </message>
+    <message>
+        <source>Count and read to 20</source>
+        <translation>Сосчитай до 20 и прочитай</translation>
+    </message>
+    <message>
+        <source>Read the clock, full hours</source>
+        <translation>Посмотри время, полные часы</translation>
+    </message>
+    <message>
+        <source>Read the clock, half hours</source>
+        <translation>Посмотри время, половинные часы</translation>
+    </message>
+    <message>
+        <source>Read the clock</source>
+        <translation>Посмотри время</translation>
+    </message>
+    <message>
+        <source>Read</source>
+        <translation>Читай</translation>
+    </message>
+    <message>
+        <source>Count</source>
+        <translation>Считай</translation>
+    </message>
+    <message>
+        <source>Clock</source>
+        <translation>Часы</translation>
+    </message>
+    <message>
+        <source>Music</source>
+        <translation>Ноты</translation>
+    </message>
+    <message>
+        <source>Read notes, whole step</source>
+        <translation>Прочитай ноты, полный тон</translation>
+    </message>
+    <message>
+        <source>Read notes, half-step</source>
+        <translation>Прочитай ноты, полутоны</translation>
+    </message>
+    <message>
+        <source>Mixed</source>
+        <translation>Смешанные</translation>
+    </message>
+    <message>
+        <source>Mixed lessons, easy</source>
+        <translation>Смешанные уроки, лёгкие</translation>
+    </message>
+    <message>
+        <source>Mixed lessons, medium</source>
+        <translation>Смешанные уроки, средние</translation>
+    </message>
+    <message>
+        <source>Mixed lessons, hard</source>
+        <translation>Смешанные уроки, трудные</translation>
+    </message>
+    <message>
+        <source>Color</source>
+        <translation>Цвет</translation>
+    </message>
+    <message>
+        <source>Recognize the color</source>
+        <translation>Распознай цвет</translation>
+    </message>
+</context>
+<context>
+    <name>Notes</name>
+    <message>
+        <source>C</source>
+        <translation>до</translation>
+    </message>
+    <message>
+        <source>C sharp</source>
+        <translation>до-диез</translation>
+    </message>
+    <message>
+        <source>D flat</source>
+        <translation>ре-бемоль</translation>
+    </message>
+    <message>
+        <source>D</source>
+        <translation>ре</translation>
+    </message>
+    <message>
+        <source>D sharp</source>
+        <translation>ре-диез</translation>
+    </message>
+    <message>
+        <source>E flat</source>
+        <translation>ми-бемоль</translation>
+    </message>
+    <message>
+        <source>E</source>
+        <translation>ми</translation>
+    </message>
+    <message>
+        <source>F flat</source>
+        <translation>фа-бемоль</translation>
+    </message>
+    <message>
+        <source>E sharp</source>
+        <translation>ми-диез</translation>
+    </message>
+    <message>
+        <source>F</source>
+        <translation>фа</translation>
+    </message>
+    <message>
+        <source>F sharp</source>
+        <translation>фа-диез</translation>
+    </message>
+    <message>
+        <source>G flat</source>
+        <translation>соль-бемоль</translation>
+    </message>
+    <message>
+        <source>G</source>
+        <translation>соль</translation>
+    </message>
+    <message>
+        <source>G sharp</source>
+        <translation>соль-диез</translation>
+    </message>
+    <message>
+        <source>A flat</source>
+        <translation>ля-бемоль</translation>
+    </message>
+    <message>
+        <source>A</source>
+        <translation>ля</translation>
+    </message>
+    <message>
+        <source>A sharp</source>
+        <translation>ля-диез</translation>
+    </message>
+    <message>
+        <source>B flat</source>
+        <translation>си-бемоль</translation>
+    </message>
+    <message>
+        <source>B</source>
+        <translation>си</translation>
+    </message>
+    <message>
+        <source>C flat</source>
+        <translation>до-бемоль</translation>
+    </message>
+</context>
+<context>
+    <name>Numbers</name>
+    <message>
+        <source>zero</source>
+        <translation>ноль</translation>
+    </message>
+    <message>
+        <source>one</source>
+        <translation>один</translation>
+    </message>
+    <message>
+        <source>two</source>
+        <translation>два</translation>
+    </message>
+    <message>
+        <source>three</source>
+        <translation>три</translation>
+    </message>
+    <message>
+        <source>four</source>
+        <translation>четыре</translation>
+    </message>
+    <message>
+        <source>five</source>
+        <translation>пять</translation>
+    </message>
+    <message>
+        <source>six</source>
+        <translation>шесть</translation>
+    </message>
+    <message>
+        <source>seven</source>
+        <translation>семь</translation>
+    </message>
+    <message>
+        <source>eight</source>
+        <translation>восемь</translation>
+    </message>
+    <message>
+        <source>nine</source>
+        <translation>девять</translation>
+    </message>
+    <message>
+        <source>ten</source>
+        <translation>десять</translation>
+    </message>
+    <message>
+        <source>eleven</source>
+        <translation>одиннадцать</translation>
+    </message>
+    <message>
+        <source>twelve</source>
+        <translation>двенадцать</translation>
+    </message>
+    <message>
+        <source>thirteen</source>
+        <translation>тринадцать</translation>
+    </message>
+    <message>
+        <source>fourteen</source>
+        <translation>четырнадцать</translation>
+    </message>
+    <message>
+        <source>fifteen</source>
+        <translation>пятнадцать</translation>
+    </message>
+    <message>
+        <source>sixteen</source>
+        <translation>шестнадцать</translation>
+    </message>
+    <message>
+        <source>seventeen</source>
+        <translation>семнадцать</translation>
+    </message>
+    <message>
+        <source>eighteen</source>
+        <translation>восемнадцать</translation>
+    </message>
+    <message>
+        <source>nineteen</source>
+        <translation>девятнадцать</translation>
+    </message>
+    <message>
+        <source>twenty</source>
+        <translation>двадцать</translation>
+    </message>
+</context>
+<context>
+    <name>Objects</name>
+    <message>
+        <source>banana</source>
+        <translation>банан</translation>
+    </message>
+    <message>
+        <source>elephant</source>
+        <translation>слон</translation>
+    </message>
+    <message>
+        <source>robot</source>
+        <translation>робот</translation>
+    </message>
+    <message>
+        <source>flower</source>
+        <translation>цветок</translation>
+    </message>
+    <message>
+        <source>fish</source>
+        <translation>рыба</translation>
+    </message>
+    <message>
+        <source>rooster</source>
+        <translation>петух</translation>
+    </message>
+    <message>
+        <source>airplane</source>
+        <translation>самолёт</translation>
+    </message>
+    <message>
+        <source>candle</source>
+        <translation>свеча</translation>
+    </message>
+    <message>
+        <source>scissors</source>
+        <translation>ножницы</translation>
+    </message>
+    <message>
+        <source>key</source>
+        <translation>ключ</translation>
+    </message>
+    <message>
+        <source>horse</source>
+        <translation>лошадь</translation>
+    </message>
+    <message>
+        <source>dog</source>
+        <translation>собака</translation>
+    </message>
+    <message>
+        <source>cat</source>
+        <translation>кошка</translation>
+    </message>
+    <message>
+        <source>camel</source>
+        <translation>верблюд</translation>
+    </message>
+    <message>
+        <source>crocodile</source>
+        <translation>крокодил</translation>
+    </message>
+    <message>
+        <source>pig</source>
+        <translation>свинья</translation>
+    </message>
+    <message>
+        <source>snake</source>
+        <translation>змея</translation>
+    </message>
+    <message>
+        <source>giraffe</source>
+        <translation>жираф</translation>
+    </message>
+    <message>
+        <source>snail</source>
+        <translation>улитка</translation>
+    </message>
+    <message>
+        <source>hedgehog</source>
+        <translation>ёж</translation>
+    </message>
+</context>
+</TS>
